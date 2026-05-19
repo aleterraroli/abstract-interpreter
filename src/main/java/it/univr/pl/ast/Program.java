fa8b1296a4030943ca9a8fd7eab0490f8ebf121f
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-19 20:30:33 +0200

    feat: ast program class

---
+++ src/main/java/it/univr/pl/ast/Program.java
@@ -0,0 +1,14 @@
+package it.univr.pl.ast;
+
+public class Program {
+
+    private final List<Statement> statements;
+
+    public Program(List<Statement> statements) {
+        this.statements = statements;
+    }
+
+    public List<Statement> getStatements() {
+        return statements;
+    }
+}
