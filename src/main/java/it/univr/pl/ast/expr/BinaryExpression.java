823223e9f0b833ee0f0f6d00e2be154a7728a526
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-19 20:39:04 +0200

    feat: binaryExpression class added

---
+++ src/main/java/it/univr/pl/ast/expr/BinaryExpression.java
@@ -0,0 +1,32 @@
+package it.univr.pl.ast.expr;
+
+import it.univr.pl.ast.operator.BinaryOperator;
+
+public class BinaryExpression extends Expression {
+
+    private final Expression left;
+    private final Expression right;
+    private final BinaryOperator operator;
+
+    public BinaryExpression(
+            Expression left,
+            BinaryOperator operator,
+            Expression right) {
+
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
+    }
+
+    public Expression getLeft() {
+        return left;
+    }
+
+    public Expression getRight() {
+        return right;
+    }
+
+    public BinaryOperator getOperator() {
+        return operator;
+    }
+}
