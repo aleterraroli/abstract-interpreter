b6aba2dec9ee57748cc7b5882b7aecd084eaa23b
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 09:29:59 +0200

    feat: toString added to unaryExpression class

--- src/main/java/it/univr/pl/ast/expr/UnaryExpression.java
+++ src/main/java/it/univr/pl/ast/expr/UnaryExpression.java
@@ -1,13 +1,36 @@
 package it.univr.pl.ast.expr;
 import it.univr.pl.ast.operator.UnaryOperator;
 
-public class UnaryExpression extends Expression {
+public class UnaryExpression
+        extends Expression {
 
     private final UnaryOperator operator;
+
     private final Expression expression;
 
-    public UnaryExpression(UnaryOperator operator, Expression expression) {
+    public UnaryExpression(
+            UnaryOperator operator,
+            Expression expression) {
+
         this.operator = operator;
         this.expression = expression;
+    }
+
+    public UnaryOperator getOperator() {
+        return operator;
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    @Override
+    public String toString() {
+
+        return "(" +
+                operator +
+                " " +
+                expression +
+                ")";
     }
 }
