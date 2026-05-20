58122a8bf740e8de64cbf02f02e55abcb7d74373
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 09:33:07 +0200

    feat: toString added to program class

--- src/main/java/it/univr/pl/ast/Program.java
+++ src/main/java/it/univr/pl/ast/Program.java
@@ -1,17 +1,35 @@
 package it.univr.pl.ast;
 
 import it.univr.pl.ast.stmt.Statement;
+
 import java.util.List;
 
 public class Program {
 
     private final List<Statement> statements;
 
-    public Program(List<Statement> statements) {
+    public Program(
+            List<Statement> statements) {
+
         this.statements = statements;
     }
 
     public List<Statement> getStatements() {
         return statements;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb =
+                new StringBuilder();
+
+        for (Statement stmt : statements) {
+
+            sb.append(stmt)
+                    .append("\n");
+        }
+
+        return sb.toString();
     }
 }
