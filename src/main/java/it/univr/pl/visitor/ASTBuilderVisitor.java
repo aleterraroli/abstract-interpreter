0740d8910fe1d88aff44a171883c1ca934baba74
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 10:13:21 +0200

    feat: visitMain method added

--- src/main/java/it/univr/pl/visitor/ASTBuilderVisitor.java
+++ src/main/java/it/univr/pl/visitor/ASTBuilderVisitor.java
@@ -1,6 +1,7 @@
 package it.univr.pl.visitor;
 
 import it.univr.pl.AbsBaseVisitor;
+import it.univr.pl.AbsParser;
 import it.univr.pl.ast.*;
 import it.univr.pl.ast.expr.*;
 import it.univr.pl.ast.operator.*;
@@ -9,6 +10,16 @@ import it.univr.pl.ast.stmt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ASTBuilderVisitor
-        extends AbsBaseVisitor<Object> {
+public class ASTBuilderVisitor extends AbsBaseVisitor<Object> {
+
+    @Override
+    public Object visitMain(AbsParser.MainContext ctx) {
+        List<Statement> statements = new ArrayList<>();
+        for(AbsParser.ComContext com : ctx.com()) {
+            statements.add((Statement) visit(com));
+        }
+        return new Program(statements);
+    }
+
+
 }
