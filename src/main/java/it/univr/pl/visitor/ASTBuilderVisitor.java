07ed3edfa096372a81363d4b2e31e41d4a512cdb
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 10:10:24 +0200

    feat: ASTbuilderVisitor class added

---
+++ src/main/java/it/univr/pl/visitor/ASTBuilderVisitor.java
@@ -0,0 +1,14 @@
+package it.univr.pl.visitor;
+
+import it.univr.pl.AbsBaseVisitor;
+import it.univr.pl.ast.*;
+import it.univr.pl.ast.expr.*;
+import it.univr.pl.ast.operator.*;
+import it.univr.pl.ast.stmt.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ASTBuilderVisitor
+        extends AbsBaseVisitor<Object> {
+}
