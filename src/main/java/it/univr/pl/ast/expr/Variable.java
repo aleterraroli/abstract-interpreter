dccf684a587876e697a9e359f6d850cec8c3c5a3
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 09:32:28 +0200

    feat: toString added to variable class

--- src/main/java/it/univr/pl/ast/expr/Variable.java
+++ src/main/java/it/univr/pl/ast/expr/Variable.java
@@ -11,4 +11,9 @@ public class Variable extends Expression {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
