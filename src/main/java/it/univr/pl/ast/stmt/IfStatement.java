1463c29c3c7752dbc249c2e7b5b12c06cab7b545
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-20 09:27:54 +0200

    feat: ifStatement added

---
+++ src/main/java/it/univr/pl/ast/stmt/IfStatement.java
@@ -0,0 +1,53 @@
+package it.univr.pl.ast.stmt;
+
+import it.univr.pl.ast.expr.Expression;
+
+public class IfStatement extends Statement {
+
+    private final Expression condition;
+
+    private final Statement thenBranch;
+
+    private final Statement elseBranch;
+
+    public IfStatement(
+            Expression condition,
+            Statement thenBranch,
+            Statement elseBranch) {
+
+        this.condition = condition;
+        this.thenBranch = thenBranch;
+        this.elseBranch = elseBranch;
+    }
+
+    public Expression getCondition() {
+        return condition;
+    }
+
+    public Statement getThenBranch() {
+        return thenBranch;
+    }
+
+    public Statement getElseBranch() {
+        return elseBranch;
+    }
+
+    @Override
+    public String toString() {
+
+        if (elseBranch == null) {
+
+            return "if (" +
+                    condition +
+                    ") " +
+                    thenBranch;
+        }
+
+        return "if (" +
+                condition +
+                ") " +
+                thenBranch +
+                " else " +
+                elseBranch;
+    }
+}
