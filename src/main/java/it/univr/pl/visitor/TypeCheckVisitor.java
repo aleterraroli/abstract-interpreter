0b2c155e8224e4a9464c7a124b52cb4ff4b47e92
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-26 11:32:13 +0200

    feat: typeCheckVisitor class base methods added

--- src/main/java/it/univr/pl/visitor/TypeCheckVisitor.java
+++ src/main/java/it/univr/pl/visitor/TypeCheckVisitor.java
@@ -1,4 +1,72 @@
 package it.univr.pl.visitor;
 
-public class TypeCheckVisitor {
+import it.univr.pl.ast.*;
+import it.univr.pl.ast.expr.*;
+import it.univr.pl.ast.stmt.*;
+import it.univr.pl.type.*;
+import it.univr.pl.exception.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeCheckVisitor implements ASTVisitor<Type> {
+
+    @Override
+    public Type visit(Program program) {
+        return null;
+    }
+
+    @Override
+    public Type visit(BinaryExpression expr) {
+        return null;
+    }
+
+    @Override
+    public Type visit(UnaryExpression expr) {
+        return null;
+    }
+
+    @Override
+    public Type visit(Variable expr) {
+        return null;
+    }
+
+    @Override
+    public Type visit(IntLiteral expr) {
+        return null;
+    }
+
+    @Override
+    public Type visit(BoolLiteral expr) {
+        return null;
+    }
+
+    @Override
+    public Type visit(DeclarationStatement stmt) {
+        return null;
+    }
+
+    @Override
+    public Type visit(AssignStatement stmt) {
+        return null;
+    }
+
+    @Override
+    public Type visit(IfStatement stmt) {
+        return null;
+    }
+
+    @Override
+    public Type visit(WhileStatement stmt) {
+        return null;
+    }
+
+    @Override
+    public Type visit(PrintStatement stmt) {
+        return null;
+    }
+
+    @Override
+    public Type visit(BlockStatement stmt) {
+        return null;
+    }
 }
