9940ac3f87b57995124505c73815ea97c5459b99
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-31 10:44:48 +0200

    feat: visitMain and visitDecl methods added

--- src/main/java/it/univr/pl/visitor/SignAbsIntp.java
+++ src/main/java/it/univr/pl/visitor/SignAbsIntp.java
@@ -1,7 +1,10 @@
 package it.univr.pl.visitor;
 
 import it.univr.pl.AbsBaseVisitor;
+import it.univr.pl.AbsParser;
 import it.univr.pl.SignAbsMem;
+import it.univr.pl.value.ComValue;
+import it.univr.pl.value.SignValue;
 import it.univr.pl.value.Value;
 
 public class SignAbsIntp extends AbsBaseVisitor<Value> {
@@ -11,5 +14,22 @@ public class SignAbsIntp extends AbsBaseVisitor<Value> {
         this.mem = mem;
     }
 
+    public SignAbsMem getMem() {
+        return mem;
+    }
 
+    @Override
+    public Value visitMain(AbsParser.MainContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    @Override
+    public Value visitDecl(AbsParser.DeclContext ctx) {
+        String id = ctx.ID().getText();
+        SignValue initialSign = SignValue.BOTTOM;
+        if(ctx.exp() != null)
+            initialSign = (SignValue) visit(ctx.exp());
+        mem.add(id, initialSign);
+        return ComValue.INSTANCE;
+    }
 }
