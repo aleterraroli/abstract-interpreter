ac0942d09ae4b23cf6ee15633a9d69db680b71a1
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-31 10:23:50 +0200

    feat: signType class added

---
+++ src/main/java/it/univr/pl/type/SignType.java
@@ -0,0 +1,34 @@
+package it.univr.pl.type;
+
+public enum SignType implements ExpType{
+    BOTTOM("⊥"),
+    ZERO("0"),
+    POS("+"),
+    NEG("-"),
+    TOP("⊤");
+
+    private final String name;
+
+    SignType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isCompatible(Type other) {
+        if (other == SimpleType.INT) return true;
+        return this == other || other == TOP;
+    }
+
+    public SignType lub(SignType other) {
+        if (this == BOTTOM) return other;
+        if (other == BOTTOM) return this;
+        if (this == TOP || other == TOP) return TOP;
+        if (this == other) return this;
+        return TOP;
+    }
+}
---
+++ src/main/java/it/univr/pl/value/SignValue.java
@@ -0,0 +1,7 @@
+package it.univr.pl.value;
+
+import it.univr.pl.type.ExpType;
+
+public enum SignValue implements ExpValue<?> {
+
+}
