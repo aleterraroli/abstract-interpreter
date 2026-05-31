464187fb9136fb566b9df16bfd733269273302c5
Chikyu-no-tsubasa <aleterraroli@gmail.com>
2026-05-31 10:25:13 +0200

    feat: signValue class added

--- src/main/java/it/univr/pl/value/SignValue.java
+++ src/main/java/it/univr/pl/value/SignValue.java
@@ -1,7 +1,22 @@
 package it.univr.pl.value;
 
-import it.univr.pl.type.ExpType;
+import it.univr.pl.type.SignType;
 
-public enum SignValue implements ExpValue<?> {
+public class SignValue extends ExpValue<SignType> {
 
+    public SignValue(SignType value) {
+        super(value);
+    }
+
+    // Scorciatoie statiche per l'interprete astratto
+    public static final SignValue BOTTOM = new SignValue(SignType.BOTTOM);
+    public static final SignValue ZERO = new SignValue(SignType.ZERO);
+    public static final SignValue POS = new SignValue(SignType.POS);
+    public static final SignValue NEG = new SignValue(SignType.NEG);
+    public static final SignValue TOP = new SignValue(SignType.TOP);
+
+    // Metodo di utilità per fare il LUB direttamente tra SignValue
+    public SignValue lub(SignValue other) {
+        return new SignValue(this.toJavaValue().lub(other.toJavaValue()));
+    }
 }
